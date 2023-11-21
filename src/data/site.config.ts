b736ac2d82ac8063a208cac1dcb1124ba399cf01
EvilAdd <meneses.di.a@gmail.com>
2023-11-20 23:00:57 -0300

    Update website URLs and translations

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	author: 'Diego Meneses', // Site author
+	title: 'Blog de Diego Meneses', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	lang: 'es-CL',
+	ogLocale: 'es-CL',
+	shareMessage: 'Comparte este post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
